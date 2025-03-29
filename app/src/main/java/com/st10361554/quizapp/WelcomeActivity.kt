8b177d9c0e93489f7dc4dde3d6e611c5d0846c8d
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2025-03-29 15:04:30 +0200

    Added LoginActivity and conditional navigation.
    - Added `LoginActivity` with basic UI.
    - Added logic to `WelcomeActivity` to navigate to either `MainActivity` (if logged in) or `LoginActivity` (if not).
    - Updated `AndroidManifest.xml` to include `LoginActivity` and set `WelcomeActivity` as the launcher activity.

--- app/src/main/java/com/st10361554/quizapp/WelcomeActivity.kt
+++ app/src/main/java/com/st10361554/quizapp/WelcomeActivity.kt
@@ -1,5 +1,6 @@
 package com.st10361554.quizapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -31,6 +32,21 @@ class WelcomeActivity : AppCompatActivity() {
         // Next button click
         binding.btnNext.setOnClickListener{
 
+            // check if user is logged in
+            if (currentUser != null)
+            {
+                // if user is logged in, navigate to main activity
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Finish so that user can't go back to splash screen
+            }
+            else
+            {
+                // if user is not logged in, navigate to login activity
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish() // Finish so that user can't go back to splash screen
+            }
         }
 
 
